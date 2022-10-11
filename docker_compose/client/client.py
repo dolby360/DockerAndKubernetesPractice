1af3be04e611e008ac480f54a2c082b056a53785
dolby360 <dolevben054@gmail.com>
2022-10-11 22:06:16 +0300

    make docker compose works

--- docker_compose/client/client.py
+++ docker_compose/client/client.py
@@ -3,11 +3,14 @@ import socket
 HOST = "backend"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-input("press any key to continue")
+while True:
+    input("press any key to continue")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b"Hello, world")
+        data = s.recv(1024)
 
-print(f"Received {data!r}")
+    print("====================")
+    print(f"Received {data!r}")
+    print("====================")
--- docker_compose/server/server.py
+++ docker_compose/server/server.py
@@ -1,16 +1,34 @@
+import mysql.connector
 import socket
 
 HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+while True:
+    print("hellp")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+    print("ssss")
+    mydb = mysql.connector.connect(
+        host="database",
+        user="root",
+        password="some_password",
+        database="demo"
+    )
+
+    mycursor = mydb.cursor()
+    print("here")
+    mycursor.execute("SELECT * FROM Persons")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
